dc44ca1ed38b733e5e16cc813acc6c9529f1d6b2
FMFS87 <83195968+FMFS87@users.noreply.github.com>
2023-09-20 21:10:14 -0700

    Lab02 - COMP2501
    Lab02a being created, with Java project being established without any
    classes.

---
+++ COMP2501/Labs/a01181350_lab02a/src/a01181350_lab02a/BankAccount.java
@@ -0,0 +1,11 @@
+//Fellipe Matheus Fumagali Scirea - A01181350
+package a01181350_lab02a;
+
+public class BankAccount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
