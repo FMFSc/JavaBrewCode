84869d9a83473ff14a176dcaf85a2d81f814044a
FMFS87 <83195968+FMFS87@users.noreply.github.com>
2023-09-20 21:41:03 -0700

    BankAccount class being create, with different objects and getters.

--- COMP2501/Labs/a01181350_lab02a/src/BankAccount.java
+++ COMP2501/Labs/a01181350_lab02a/src/BankAccount.java
@@ -1,9 +1,35 @@
+//Fellipe Matheus Fumagali Scirea - S.N. #A01181350
 
 public class BankAccount {
+	private String memberLastName;
+	private int accountNumber;
+	private float balanceCdn;
+	
+	public BankAccount(String memberLastName, int accountNumber, float balanceCdn) {
+		super();
+		this.memberLastName = memberLastName;
+		this.accountNumber = accountNumber;
+		this.balanceCdn = balanceCdn;
+	}
+
+	public String getMemberLastName() {
+		return memberLastName;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public float getBalanceCdn() {
+		return balanceCdn;
+	}
+
+
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
